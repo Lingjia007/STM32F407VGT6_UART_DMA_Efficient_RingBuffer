5ded4af9c1b483bd197b61d8ef99513fbc8f6518
Lingjia007 <3125497423@qq.com>
2025-10-17 14:12:56 +0800

    first commit

---
+++ MDK-ARM/RTE/_STM32F407VGT6_UART_DMA_Efficient_RingBuffer/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F407VGT6_UART_DMA_Efficient_RingBuffer' 
+ * Target:  'STM32F407VGT6_UART_DMA_Efficient_RingBuffer' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/STM32F407VGT6_UART_DMA_Efficient_RingBuffer/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+stm32f407vgt6_uart_dma_efficient_ringbuffer\startup_stm32f407xx.o: startup_stm32f407xx.s
